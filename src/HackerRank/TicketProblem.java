4a27bcb3a1826dd06f7759ef6bbe0eb0ce80aa4c
Nikhil Bhirud <nbhirud@gmail.com>
2017-04-28 20:13:20 -0400

    v1.4

---
+++ src/HackerRank/TicketProblem.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HackerRank;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author nbhirud
+ */
+
+public class TicketProblem {
+
+    public static void main(String[] args) {
+        int[] list = {4, 13, 2, 3};
+        int max = maxTickets(list);
+        System.out.println(max);
+    }
+
+    static int maxTickets(int[] tickets) {
+        int max = 1;
+        int currMax = 1;
+        int pastNum;
+        Arrays.sort(tickets);
+        if (tickets.length == 0) {
+            return 0;
+        } else {
+            pastNum = tickets[0];
+        }
+        for (int i = 1; i < tickets.length; i++) {
+            if (Math.abs(tickets[i] - pastNum) < 2) {
+                currMax++;
+            } else {
+                currMax = 1;
+            }
+            pastNum = tickets[i];
+            if (max < currMax) {
+                max = currMax;
+            }
+        }
+        return max;
+    }
+}
